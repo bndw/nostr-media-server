b902bdb3273927af6761bcc2b534b0bc7426980c
Ben Woodward <ben@bdw.to>
2023-02-03 09:41:36 -0800

    feat: configure max upload size (#1)
    * feat: configure max upload size
    
    * ci: Update actions go

--- config.go
+++ config.go
@@ -7,6 +7,10 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+const (
+	defaultMaxUploadSizeMB = 2
+)
+
 type Config struct {
 	// Spec config
 	APIPath             string            `yaml:"api_path" envconfig:"API_PATH"`
@@ -17,9 +21,10 @@ type Config struct {
 	Names               map[string]string `yaml:"names" envconfig:"NAMES"`
 
 	// Server config
-	Port          int               `yaml:"port" envconfig:"PORT"`
-	StorageType   string            `yaml:"storage_type" envconfig:"STORAGE_TYPE"`
-	StorageConfig map[string]string `yaml:"storage_config" envconfig:"STORAGE_CONFIG"`
+	Port            int               `yaml:"port" envconfig:"PORT"`
+	StorageType     string            `yaml:"storage_type" envconfig:"STORAGE_TYPE"`
+	StorageConfig   map[string]string `yaml:"storage_config" envconfig:"STORAGE_CONFIG"`
+	MaxUploadSizeMB int64             `yaml:"max_upload_size_mb" envconfig:"MAX_UPLOAD_SIZE_MB"`
 }
 
 // Load Config from a yaml file at path.
@@ -30,10 +35,19 @@ func (c *Config) Load(path string) error {
 	}
 	defer f.Close()
 
+	c.applyDefaults()
+
 	return yaml.NewDecoder(f).Decode(c)
 }
 
 // Load Config from the environment.
 func (c *Config) LoadFromEnv() error {
+	c.applyDefaults()
 	return envconfig.Process("", c)
+}
+
+func (c *Config) applyDefaults() {
+	if c.MaxUploadSizeMB == 0 {
+		c.MaxUploadSizeMB = defaultMaxUploadSizeMB
+	}
 }
